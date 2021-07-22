1b464ecc3028ad87d59cce0f2d13bf34628cea56
paulo.piotto <paulo_piotto@hotmail.com>
2021-07-22 20:35:17 -0300

    layout basico finalizado

---
+++ script.js
@@ -0,0 +1,7 @@
+function selecionaItem(){
+    let item = document.getElementById("item");
+    item.style.border = "5px solid #32b72f";
+
+    let check = document.getElementById("check");
+    check.style.display = "initial";
+}
