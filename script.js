df7f464387abda8909ccc388084d8c3912bb37ea
paulo.piotto <paulo_piotto@hotmail.com>
2021-07-24 12:51:48 -0300

    selecao de itens finalizada

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-function selecionaItem(item){
-   item.classList.toggle("selecionado");
+function selecionaPrato(item){
+   let selecionado = document.querySelector(".pratos .selecionado");
 
+   if(selecionado !== null){
+      selecionado.classList.remove("selecionado");
+   }
+
+      item.classList.add("selecionado");
+
+      return console.log(item.querySelector(".nome-item").innerHTML);
+   
+}
+
+function selecionaBebida(item){
+   let selecionado = document.querySelector(".bebidas .selecionado");
+
+   if(selecionado !== null){
+      selecionado.classList.remove("selecionado");
+   }
+
+      item.classList.add("selecionado");
+   
+      return console.log(item.querySelector(".nome-item").innerHTML);
+
+}
+
+function selecionaSobremesa(item){
+   let selecionado = document.querySelector(".sobremesas .selecionado");
+
+   if(selecionado !== null){
+      selecionado.classList.remove("selecionado");
+   }
+
+      item.classList.add("selecionado");
+   
+      return console.log(item.querySelector(".nome-item").innerHTML);
+
+}
+
+function liberaPedido(){
+   
 }
